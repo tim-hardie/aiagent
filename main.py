43f8d9e211db1cae5a90db9dace7045010d52bff
tim-hardie <timothy.g.hardie@gmail.com>
2025-08-02 11:36:32 +0100

    create basic AI agent in CLI using Gemini

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: no prompt provided")
+        sys.exit(1)
+    else:
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
